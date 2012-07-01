f9c6a166f63b91d2fb394f044fde2d7bc7272409
Jan Deinhard <jd@one.(none)>
2012-07-01 22:49:00 +0200

    initial commit

---
+++ server/app.js
@@ -0,0 +1,28 @@
+var express = require('express')
+  , routes = require('./routes')
+  , fs = require('fs');
+
+var app = module.exports = express.createServer();
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
